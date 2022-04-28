dc8f6324425d9323ef43a558d2886ab24502130f
Richard Burkhill <reburkhill@gmail.com>
2022-04-28 21:48:36 +0100

    Improved polymorphic behaviour

--- C++/unique_ptr.cpp
+++ C++/unique_ptr.cpp
@@ -6,23 +6,31 @@
 using namespace std;
 
 class Dog {
-   void Bark(){cout<< "I want to be the boss";}
+public:
+   void Bark(){cout<< "I want to be the boss"<< endl;}
    virtual void complain () = 0;
 };
 
-class British_BUlldog : public Dog {
+class British_Bulldog : public Dog {
+public:
    void complain () {cout << "I waddle when I walk, I don't like it so much" << endl;}
 };
 
 class French_Pickanees : public Dog {
-   void complain () {cout << "I will pick your knees if my devotee gives you too much attention";}
+public:
+   void complain () {cout << "I will pick your knees if my devotee gives you too much attention"<<endl;}
 };
-
-std::unique_ptr<Dog> k911;
 
 int main(){
 
-   unique_ptr<Dog> ubbd = std::make_unique<British_BUlldog>();
-   unique_ptr<Dog> ufpn = std::make_unique<French_Pickanees>();
-
+   unique_ptr<British_Bulldog> ubbd = std::make_unique<British_Bulldog>();
+   unique_ptr<French_Pickanees> ufpn = std::make_unique<French_Pickanees>();
+   
+   ubbd->complain();
+   ufpn->complain();
+   
+   std::unique_ptr<Dog> k911;
+   
+   k911 = std::move(ufpn);
+   k911->complain();
 }
